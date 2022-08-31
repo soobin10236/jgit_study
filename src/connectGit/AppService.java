aa2749bbd254e0228017c8629386a43d18de2122
tnqls7742 <tnqls7742@douzone.com>
2022-08-31 17:58:29 +0900

    git properties

--- src/connectGit/AppService.java
+++ src/connectGit/AppService.java
@@ -7,9 +7,13 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.Ref;
@@ -20,81 +24,26 @@ import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 
 
 public class AppService {
-
 	
-	public static void main(String[] args) throws IOException, GitAPIException {
+	public static final String localPath = "C:\\Users\\LSB\\Desktop\\test_repo";
+	public static final String username = "tnqls7742";
+	public static final String password = "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";
+	
+	public static void main(String[] args) throws Exception {
 		
 		System.out.println("started");
 		
-//		GitService gitService = new GitService();
-//		
-////		gitService.createRepository(); //git clone
-//		
-//		gitService.openGit();
+		GitService gitService = new GitService();
 		
-		File localPath = File.createTempFile("JGitTestRepository", "");
-        // delete repository before running this
-        Files.delete(localPath.toPath());
+//		gitService.callGitService();
+		
+		gitService.apiTestGet();
 
-        // This code would allow to access an existing repository
-//      try (Git git = Git.open(new File("/home/vogella/git/eclipse.platform.ui"))) {
-//          Repository repository = git.getRepository();
-//
-//      }
-
-        // Create the git repository with init
-        try (Git git = Git.init().setDirectory(new File("C:\\Users\\LSB\\Desktop\\test_repo")).call()) {
-            System.out.println("Created repository: " + git.getRepository().getDirectory());
-            File myFile = new File(git.getRepository().getDirectory().getParent(), "testfile");
-            if (!myFile.createNewFile()) {
-                throw new IOException("Could not create file " + myFile);
-            }
-
-            // run the add-call
-            git.add().addFilepattern("testfile").call();
-
-            git.commit().setMessage("Initial commit").call();
-            System.out.println("Committed file " + myFile + " to repository at " + git.getRepository().getDirectory());
-            // Create a few branches for testing
-            for (int i = 0; i < 10; i++) {
-                git.checkout().setCreateBranch(true).setName("new-branch" + i).call();
-            }
-            // List all branches
-            List<Ref> call = git.branchList().call();
-            for (Ref ref : call) {
-                System.out.println("Branch: " + ref + " " + ref.getName() + " " + ref.getObjectId().getName());
-            }
-
-            // Create a few new files
-            for (int i = 0; i < 10; i++) {
-                File f = new File(git.getRepository().getDirectory().getParent(), "testfile" + i);
-                f.createNewFile();
-                if (i % 2 == 0) {
-                    git.add().addFilepattern("testfile" + i).call();
-                }
-            }
-
-            Status status = git.status().call();
-
-            Set<String> added = status.getAdded();
-            for (String add : added) {
-                System.out.println("Added: " + add);
-            }
-            Set<String> uncommittedChanges = status.getUncommittedChanges();
-            for (String uncommitted : uncommittedChanges) {
-                System.out.println("Uncommitted: " + uncommitted);
-            }
-
-            Set<String> untracked = status.getUntracked();
-            for (String untrack : untracked) {
-                System.out.println("Untracked: " + untrack);
-            }
-
-            // Find the head for the repository
-            ObjectId lastCommitId = git.getRepository().resolve(Constants.HEAD);
-            System.out.println("Head points to the following commit :" + lastCommitId.getName());
-        }
-		System.out.println("finished...");
+		
+			
+		
+		System.out.println("finished");
+		
 	}
 
 }
--- src/connectGit/GitService.java
+++ src/connectGit/GitService.java
@@ -1,12 +1,22 @@
 package connectGit;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import org.eclipse.jgit.api.AddCommand;
+import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.errors.CheckoutConflictException;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRefNameException;
@@ -14,33 +24,114 @@ import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
 import org.eclipse.jgit.api.errors.RefNotFoundException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+
+import common.util;
 
 
 public class GitService {
 	
-	public Git createRepository() throws IOException, GitAPIException {
+	util util = new util();
+	Properties prop = util.readProperties("config/git.properties");
+	
+	
+	public static final String localPath = "C:\\Users\\LSB\\Desktop\\test_repo";
+	public static final String username = "tnqls7742";
+//	public static final String password = "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";
+	String password = prop.getProperty("password");
+	
+	public void callGitService() throws IOException, GitAPIException {
 		
-		Git git = Git.cloneRepository()
-					 .setURI("https://github.com/soobin10236/test_repo.git")
-					 .setDirectory(new File("C:\\Users\\LSB\\Desktop\\test_repo")) 
-					 .call();
+		Git git = Git.open(new File(localPath));
+		System.out.println(git.toString());
+		
+		AddCommand add = git.add();
+		add.addFilepattern("first_file");
+		add.call();
+		System.out.println("add call");
+		
+		CommitCommand commit = git.commit();
+		commit.setMessage("java commit");
+		commit.call();
+		System.out.println("commit call");
+		
+		PushCommand push = git.push();
+		push.setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password));		
+		push.call();
+		System.out.println("push call");
+	}
+
+	
+	public void apiTestGet() throws Exception 
+	{
+		
+		System.out.println(password);
+	    URL url = null;
+	    String readLine = null;
+	    StringBuilder buffer = null;
+	    BufferedReader bufferedReader = null;
+	    BufferedWriter bufferedWriter = null;
+	    HttpURLConnection urlConnection = null;
+	    	
+	    int connTimeout = 5000;
+	    int readTimeout = 3000;
 			
-		return git;
-	}
+	    String apiUrl = "https://jsonplaceholder.typicode.com/todos/1";	// 각자 상황에 맞는 IP & url 사용 		
+			
+	    try 
+	    {
+	        url = new URL(apiUrl);
+	        urlConnection = (HttpURLConnection)url.openConnection();
+	        urlConnection.setRequestMethod("GET");
+	        urlConnection.setConnectTimeout(connTimeout);
+	        urlConnection.setReadTimeout(readTimeout);
+	        urlConnection.setRequestProperty("Accept", "application/json;");
+				
+	        buffer = new StringBuilder();
+	        if(urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) 
+	        {
+	            bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream(),"UTF-8"));
+	            while((readLine = bufferedReader.readLine()) != null) 
+	    	    {
+	                buffer.append(readLine).append("\n");
+	            }
+	        }
+	        else 
+	        {
+	            buffer.append("code : ");
+	            buffer.append(urlConnection.getResponseCode()).append("\n");
+	            buffer.append("message : ");
+	            buffer.append(urlConnection.getResponseMessage()).append("\n");
+	        }
+	    }
+	    catch(Exception ex) 
+	    {
+	        ex.printStackTrace();
+	    }
+	    finally 
+	    {
+	        try 
+	        {
+	            if (bufferedWriter != null) { bufferedWriter.close(); }
+	            if (bufferedReader != null) { bufferedReader.close(); }
+	        }
+	        catch(Exception ex) 
+	        { 
+	            ex.printStackTrace();
+	        }
+	    }
+			
+			
+	        System.out.println("결과 : " + buffer.toString());
+	    }
 	
-	public void openGit() throws IOException, RefAlreadyExistsException, RefNotFoundException, InvalidRefNameException, CheckoutConflictException, GitAPIException {
-		
-		Git.open(new File("C:\\Users\\LSB\\Desktop\\test_repo"))
-		             .checkout().call();
-		
-	}
 	
-	public void createBranch(Git git) throws RefAlreadyExistsException, RefNotFoundException, InvalidRefNameException, CheckoutConflictException, GitAPIException {
-		git.checkout()
-		   .setCreateBranch(true)
-		   .setName("dev-tnqls7742")
-		   .call();
-	}
+	
+	
+	
+	
+	
+	
 }
 
 
